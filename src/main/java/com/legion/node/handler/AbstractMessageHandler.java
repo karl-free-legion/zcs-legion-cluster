fe5bb0758133278cc77d21e6e6c506efee5314f7
kevinkarl <10416885@qq.com>
2020-08-19 20:18:33 +0800

    见changelog

--- src/main/java/com/legion/node/handler/AbstractMessageHandler.java
+++ src/main/java/com/legion/node/handler/AbstractMessageHandler.java
@@ -5,16 +5,18 @@ import com.legion.net.common.exceptions.LegionNetException;
 import com.legion.net.entities.LegionChannelGroup;
 import com.legion.net.entities.LegionNodeContext;
 import com.legion.net.entities.LegionNodeInfo;
-import com.legion.net.entities.SyncModuleInfo;
 import com.legion.net.netty.server.message.NodeMessageProcess;
 import com.legion.net.netty.transport.LegionCourier;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.legion.net.entities.LegionNodeContext.context;
 
 @Slf4j
 public abstract class AbstractMessageHandler implements NodeMessageProcess {
@@ -47,7 +49,7 @@ public abstract class AbstractMessageHandler implements NodeMessageProcess {
      * @param nodeId
      */
     void forwardMessage(X.XMessage message, String nodeId) {
-        LegionNodeContext context = LegionNodeContext.context();
+        LegionNodeContext context = context();
         LegionNodeInfo available = context.getNodeInfoById(nodeId);
         if (available == null) {
             log.error("forward process message[header={}] to Node not existed!", message.getHeader());
@@ -65,99 +67,52 @@ public abstract class AbstractMessageHandler implements NodeMessageProcess {
         }
     }
 
-//    public boolean send(X.XMessage message, String groupId) {
-//        LegionNodeContext context = LegionNodeContext.context();
-//        LegionChannelGroup channelGroup = context.getChannelGroup();
-//        LegionNodeInfo selfInfo = context.getSelfInfo();
-//
-//        //修复module无法负载的问题
-//        List<Object> sendTarget = new ArrayList<>();
-//        List<Channel> availableChannel = channelGroup.getOneRow(groupId).stream().filter(channel -> channel != null).collect(Collectors.toList());
-//        sendTarget.addAll(availableChannel);
-//        if (!message.getHeader().hasNodeSource()) {
-//            //修复legion转发时, 排除自己
-//            //修复重复跳转的问题，有NodeSource证明是Forward过来的，本节点一定有对应的Module的连接，如果没有让客户端重试
-//            List<LegionNodeInfo> availableNode = context.getClusterNodes()
-//                    .values()
-//                    .stream()
-//                    .filter(legionNodeInfo -> legionNodeInfo.getNetInfo().compareTo(selfInfo.getNetInfo()) != 0)
-//                    .filter(LegionNodeInfo::isAlive)
-//                    .filter(n -> n.matchGroup(groupId))
-//                    .collect(Collectors.toList());
-//            sendTarget.addAll(availableNode);
-//        }
-//        if (sendTarget.size() > 0) {
-//            Collections.shuffle(sendTarget);
-//            for (Object target : sendTarget) {
-//                if (target instanceof Channel) {
-//                    Channel channel = (Channel) target;
-//                    LegionCourier.instance().sendModule(message, message.getHeader().getMsgType(), channel);
-//                    return true;
-//                } else if (target instanceof LegionNodeInfo) {
-//                    LegionNodeInfo info = (LegionNodeInfo) target;
-//                    try {
-//                        LegionCourier.instance().sendNodeForward(message, message.getHeader().getMsgType(), info.getNetInfo());
-//                        if (log.isDebugEnabled()) {
-//                            log.debug("forward message[from={},gid={}] to Node[{}]", message.getHeader().getModuleSource(), groupId, info.getNetInfo());
-//                        }
-//                        return true;
-//                    } catch (Exception ex) {
-//                        log.warn("forward message[from={},gid={}] to Node[{}] Failed!", message.getHeader().getModuleSource(), groupId, info.getNetInfo());
-//                    }
-//                }
-//            }
-//        }
-//
-//        LegionCourier.instance().sendModule(message, message.getHeader().getMsgType());
-//        return true;
-//    }
-
-    public boolean send(X.XMessage message, final String groupId) {
-        final String routeVersion = message.getHeader().getRouteVersion();
-
+    public boolean send(X.XMessage message, String groupId) {
         LegionNodeContext context = LegionNodeContext.context();
         LegionChannelGroup channelGroup = context.getChannelGroup();
         LegionNodeInfo selfInfo = context.getSelfInfo();
-        List<SyncModuleInfo> matchModules = context.getSelfInfo().getVersionMatchedModule(groupId, routeVersion);
 
-        //随机方式送到目标
-        List<Channel> sendTarget = channelGroup.getOneRowSelected(groupId, matchModules).stream().filter(channel -> channel != null).collect(Collectors.toList());
-        Collections.shuffle(sendTarget);
-//        log.warn("msg[v={}] target M [{}] channel size = {}", routeVersion, matchModules, sendTarget);
-
-        for (Channel channel : sendTarget) {
-            try {
-                LegionCourier.instance().sendModule(message, message.getHeader().getMsgType(), channel);
-                return true;
-            } catch (Exception e) {
-                log.warn("send message[from={},gid={},v={}] to Module[{},{}] failed", message.getHeader().getModuleSource(), groupId, routeVersion, channel.isActive(), channel.isWritable());
-            }
-        }
-        //本节点无直达路径，转发
+        //修复module无法负载的问题
+        List<Object> sendTarget = new ArrayList<>();
+        List<Channel> availableChannel = channelGroup.getOneRow(groupId).stream().filter(channel -> channel != null).collect(Collectors.toList());
+        sendTarget.addAll(availableChannel);
         if (!message.getHeader().hasNodeSource()) {
             //修复legion转发时, 排除自己
             //修复重复跳转的问题，有NodeSource证明是Forward过来的，本节点一定有对应的Module的连接，如果没有让客户端重试
-            List<LegionNodeInfo> forwardTarget = context.getClusterNodes()
+            List<LegionNodeInfo> availableNode = context.getClusterNodes()
                     .values()
                     .stream()
                     .filter(legionNodeInfo -> legionNodeInfo.getNetInfo().compareTo(selfInfo.getNetInfo()) != 0)
                     .filter(LegionNodeInfo::isAlive)
-                    .filter(n -> n.matchGroupVersion(groupId, routeVersion))
+                    .filter(n -> n.matchGroup(groupId))
                     .collect(Collectors.toList());
-            for (LegionNodeInfo node : forwardTarget) {
-                try {
-                    LegionCourier.instance().sendNodeForward(message, message.getHeader().getMsgType(), node.getNetInfo());
-                    log.info("forward message[from={},gid={},v={}] to Node[{}] complete", message.getHeader().getModuleSource(), groupId, routeVersion, node.getNetInfo());
+            sendTarget.addAll(availableNode);
+        }
+        if (sendTarget.size() > 0) {
+            Collections.shuffle(sendTarget);
+            for (Object target : sendTarget) {
+                if (target instanceof Channel) {
+                    Channel channel = (Channel) target;
+                    LegionCourier.instance().sendModule(message, message.getHeader().getMsgType(), channel);
                     return true;
-                } catch (Exception e) {
-                    log.warn("forward message[from={},gid={},v={}] to Node[{}] Failed", message.getHeader().getModuleSource(), groupId, routeVersion, node.getNetInfo(), e);
+                } else if (target instanceof LegionNodeInfo) {
+                    LegionNodeInfo info = (LegionNodeInfo) target;
+                    try {
+                        LegionCourier.instance().sendNodeForward(message, message.getHeader().getMsgType(), info.getNetInfo());
+                        if (log.isDebugEnabled()) {
+                            log.debug("forward message[from={},gid={}] to Node[{}]", message.getHeader().getModuleSource(), groupId, info.getNetInfo());
+                        }
+                        return true;
+                    } catch (Exception ex) {
+                        log.warn("forward message[from={},gid={}] to Node[{}] Failed!", message.getHeader().getModuleSource(), groupId, info.getNetInfo());
+                    }
                 }
             }
         }
 
-        //暂无路由，进入重试等待队列
         LegionCourier.instance().sendModule(message, message.getHeader().getMsgType());
-        log.warn("send message[from={},gid={},v={}] failed due no channel or forward", message.getHeader().getModuleSource(), groupId, routeVersion);
         return true;
     }
+
+
 }
--- src/main/java/com/legion/node/handler/SimpleSendMessageHandler.java
+++ src/main/java/com/legion/node/handler/SimpleSendMessageHandler.java
@@ -2,8 +2,20 @@ package com.legion.node.handler;
 
 import com.legion.core.api.X;
 import com.legion.core.utils.RequestURI;
+import com.legion.net.entities.LegionChannelGroup;
+import com.legion.net.entities.LegionNodeContext;
+import com.legion.net.entities.LegionNodeInfo;
+import com.legion.net.entities.SyncModuleInfo;
+import com.legion.net.netty.transport.LegionCourier;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.legion.net.entities.LegionNodeContext.context;
 
 @Slf4j
 public class SimpleSendMessageHandler extends AbstractMessageHandler {
@@ -14,7 +26,6 @@ public class SimpleSendMessageHandler extends AbstractMessageHandler {
         if (message.getHeader().getMsgType().equals(X.XMessageType.NORMAL) && !message.getIsReply()) {
             return true;
         }
-
         return !message.getIsReply() && message.getHeader().getTagType().equals(X.XTagType.PROCESS) && message.getHeader().hasNodeSource();
     }
 
@@ -22,6 +33,58 @@ public class SimpleSendMessageHandler extends AbstractMessageHandler {
     public boolean doHandler(X.XMessage message, ChannelHandlerContext channelHandlerContext) {
         RequestURI request = RequestURI.create(message.getHeader().getUri());
         String groupId = request.getGroupId();
-        return send(message, groupId);
+        try {
+            return route(message, groupId);
+        } catch (Exception e) {
+            log.error("receive message[uri={},v={},g={}] failed:", message.getHeader().getUri(), groupId, message.getHeader().getRouteVersion(), e);
+        }
+        return true;
+    }
+
+    public boolean route(X.XMessage message, final String groupId) {
+        final String routeVersion = message.getHeader().getRouteVersion();
+        LegionNodeContext context = context();
+        LegionChannelGroup channelGroup = context.getChannelGroup();
+        LegionNodeInfo selfInfo = context.getSelfInfo();
+        List<SyncModuleInfo> matchModules = selfInfo.getVersionMatchedModule(groupId, routeVersion);
+
+        //随机方式送到目标
+        List<Channel> sendTarget = channelGroup.getOneRowSelected(groupId, matchModules).stream().filter(channel -> channel != null).collect(Collectors.toList());
+        Collections.shuffle(sendTarget);
+        for (Channel channel : sendTarget) {
+            try {
+                LegionCourier.instance().sendModule(message, message.getHeader().getMsgType(), channel);
+                return true;
+            } catch (Exception e) {
+                log.warn("send message[from={},gid={},v={}] to Module[{},{}] failed", message.getHeader().getModuleSource(), groupId, routeVersion, channel.isActive(), channel.isWritable());
+            }
+        }
+//        log.debug("send message[from={},{},gid={},v={}] need to forward", message.getHeader().getModuleSource().getGroupId(), message.getHeader().getModuleSource().getModuleId(), groupId, message.getHeader().getRouteVersion());
+        //本节点无直达路径，转发
+        if (!message.getHeader().hasNodeSource()) {
+            //修复legion转发时, 排除自己
+            //修复重复跳转的问题，有NodeSource证明是Forward过来的，本节点一定有对应的Module的连接，如果没有让客户端重试
+            List<LegionNodeInfo> forwardTarget = context.getClusterNodes()
+                    .values()
+                    .stream()
+                    .filter(legionNodeInfo -> legionNodeInfo.getNetInfo().compareTo(selfInfo.getNetInfo()) != 0)
+                    .filter(LegionNodeInfo::isAlive)
+                    .filter(n -> n.matchGroupVersion(groupId, routeVersion))
+                    .collect(Collectors.toList());
+            for (LegionNodeInfo node : forwardTarget) {
+                try {
+                    LegionCourier.instance().sendNodeForward(message, message.getHeader().getMsgType(), node.getNetInfo());
+//                    log.debug("forward message[from={},{},gid={},v={}] to Node[{}] complete", message.getHeader().getModuleSource().getGroupId(), message.getHeader().getModuleSource().getModuleId(), groupId, routeVersion, node.getNetInfo());
+                    return true;
+                } catch (Exception e) {
+                    log.warn("forward message[from={},gid={},v={}] to Node[{}] Failed", message.getHeader().getModuleSource(), groupId, routeVersion, node.getNetInfo(), e);
+                }
+            }
+        }
+
+        //暂无路由，进入重试等待队列
+        LegionCourier.instance().sendModule(message, message.getHeader().getMsgType());
+        log.warn("send message[from={},gid={},v={}] failed due no channel or forward", message.getHeader().getModuleSource(), groupId, routeVersion);
+        return true;
     }
 }
